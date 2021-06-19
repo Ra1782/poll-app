e0a41a5c150ab02ccfa1216b81a176df7c3e4a44
omkarpawar2001 <64770134+omkarpawar2001@users.noreply.github.com>
2021-06-19 20:05:43 +0530

    Add files via upload

---
+++ lib/page/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gradients/flutter_gradients.dart';
+import 'package:google_signin_example/provider/google_sign_in.dart';
+import 'package:google_signin_example/widget/logged_in_widget.dart';
+import 'package:google_signin_example/widget/sign_up_widget.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(
+    decoration: BoxDecoration(
+      gradient: FlutterGradients.loveKiss(type: GradientType.linear,
+          tileMode:TileMode.clamp),),
+      //FlutterGradients.spaceShift(type: GradientType.linear),),
+    child:Scaffold(
+      backgroundColor: Colors.transparent,
+      body: ChangeNotifierProvider(
+        create: (context) => GoogleSignInProvider(),
+        child: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            final provider = Provider.of<GoogleSignInProvider>(context);
+            if (provider.isSigningIn) {
+              return buildLoading();
+            } else if (snapshot.hasData) {
+              return LoggedInWidget();
+            } else {
+              return SignUpWidget();
+            }
+          },
+        ),
+      ),
+    ),
+  );
+
+
+  Widget buildLoading() => Stack(
+    fit: StackFit.expand,
+    children: [
+      Center(child: CircularProgressIndicator()),
+    ],
+  );
+}
